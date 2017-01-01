3ea2dcfa1c6d23f8db594851acfa0489ad8d10c7
nickgulrajani <nicholasg@gmavt.net>
2017-01-01 14:17:40 -0500

    A Change

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -5,6 +5,6 @@ package artifactory.test;
  */
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hello Nick Gu");
+        System.out.println("Hello Nick Gul");
     }
 }

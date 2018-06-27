a12173b50c72759de03b99ab6a2d8b6287168f22
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-06-27 12:23:26 +0000

    Tag change

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -5,6 +5,6 @@ package artifactory.test;
  */
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println(" Exception Errors ");
+        System.out.println("Hello Java ");
     }
 }

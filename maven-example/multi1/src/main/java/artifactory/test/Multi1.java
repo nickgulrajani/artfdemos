d229e8751c0ce696439f79aef65825f0f7ac7256
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-06-27 12:33:43 +0000

    Updated tag name on featureA

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -6,5 +6,6 @@ package artifactory.test;
 public class Multi1 {
     public static void main(String[] args) {
         System.out.println("Hello Java ");
+        System.out.println("Feature A Branch");
     }
 }

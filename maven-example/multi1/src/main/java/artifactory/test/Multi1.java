7dac3414f8fb4a9ef41d358bf8d4791783722f36
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-07-05 14:14:51 +0000

    Polling SUpport

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -9,7 +9,7 @@ public class Multi1 {
         System.out.println("Feature A Branch");
         System.out.println("Feature B Branch");
         System.out.println("Feature C Branch");
-        Syetem.out.println("Webhook");
-        Syetem.out.println("Polling");
+        System.out.println("Webhook");
+        System.out.println("Polling");
 }
 }

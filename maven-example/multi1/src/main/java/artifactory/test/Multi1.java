fadc3f06ebb7dc0dfca4faeaf5dd3fe23b021058
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-07-05 13:36:50 +0000

    Feature Changes

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -7,6 +7,7 @@ public class Multi1 {
     public static void main(String[] args) {
         System.out.println("Hello Java ");
         System.out.println("Feature A Branch");
-        System.out.println(Feature B Branch");
+        System.out.println("Feature B Branch");
+        System,out.println("Feature C Branch");
     }
 }

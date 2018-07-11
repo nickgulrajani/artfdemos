0d65db6d7196822df5bc49d40975e6f72cac8a4d
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-07-11 22:41:17 +0000

    Added a data file

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -10,5 +10,6 @@ public class Multi1 {
         System.out.println("Feature B Branch");
         System.out.println("Feature C Branch");
         System.out.println("Webhook");
+        System.out.println("Added a data file");
 }
 }

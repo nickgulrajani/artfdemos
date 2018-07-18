392ebe9fc0a83ce96ea28b95c84a1ca92ca2fc8d
Gulrajani, Nicholas <nicholas.gulrajani@accenture.com>
2018-07-18 02:13:33 +0000

    Trigger Test

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -11,5 +11,6 @@ public class Multi1 {
         System.out.println("Feature C Branch");
         System.out.println("Webhook");
         System.out.println("Added a data file");
+        System.out.println(" Trigger test");
 }
 }
